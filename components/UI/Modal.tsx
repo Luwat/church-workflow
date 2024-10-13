f08e64030a4d864a28807244d78e4540fe9285a0
luwat <oluwatobioreniyi@gmail.com>
2024-10-13 20:49:29 +0100

    Create modal

---
+++ components/UI/Modal.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import React, { useEffect, useRef, useState } from 'react'
+
+interface ModalProps {
+    isOpen: boolean;
+    // hasCloseBtn?: boolean;
+    onClose?: () => void;
+    children: React.ReactNode;
+  };
+  
+const Modal = ({isOpen, onClose, children}: ModalProps) => {
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(isOpen)
+    const modalRef = useRef<HTMLDialogElement | null>(null);
+
+    useEffect(() => {
+        setIsModalOpen(isOpen)
+    }, [isOpen])
+
+    useEffect(() => {
+        const modalElement = modalRef.current;
+        if (modalElement) {
+            if (isModalOpen) {
+                modalElement.showModal()
+            } else {
+                modalElement.close()
+            }
+        }
+    }, [isModalOpen])
+
+    const handleCloseModal = () => {
+        if (onClose) {
+            onClose();
+        }
+        setIsModalOpen(false)
+    }
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLDialogElement>) => {
+        if (event.key === "Escape") {
+            handleCloseModal()
+        }
+    }
+
+  return (
+    <dialog ref={modalRef} onKeyDown={handleKeyDown}>
+        {children}
+    </dialog>
+  )
+}
+
+export default Modal
